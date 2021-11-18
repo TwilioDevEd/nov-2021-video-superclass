8ad9bc4b97c9b3e048647f1dbdef4fd7bf2c5f20
Sarah Stringer <sstringer@twilio.com>
2021-11-18 11:09:27 -0800

    Code we wrote during the Superclass.

--- static/main.js
+++ static/main.js
@@ -11,16 +11,55 @@ window.addEventListener("load", () => {
   // join the video room
   async function connect() {
     startDiv.style.display = "none";
-    // TODO: Fetch an access token
+    const response = await fetch("/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({identity: identityInput.value})
+    })
+    const { token } = await response.json()
+    console.log(token);
 
     // TODO: Use the access token to join a room
+    const room = await Twilio.Video.connect(token, {
+      video: true,
+      audio: false
+    })
+    Twilio.VideoRoomMonitor.registerVideoRoom(room);
+    Twilio.VideoRoomMonitor.openMonitor();
+    handleConnectedParticipant(room.localParticipant);
+    room.participants.forEach(handleConnectedParticipant);
+    room.on("participantConnected", handleConnectedParticipant);
+
+    room.on("participantDisconnected", handleDisconnectedParticipant);
+    window.addEventListener("pagehide", () => {room.disconnect()});
+    window.addEventListener("beforeunload", () => {room.disconnect()});
   }
 
   // TODO: Complete function for handling when a participant connects to the room
-  function handleConnectedParticipant(participant) {}
+  function handleConnectedParticipant(participant) {
+    findNextAvailableYarn(participant);
+    participant.tracks.forEach((trackPublication) => {
+      handleTrackPublished(trackPublication, participant);
+    });
+    participant.on("trackPublished", (trackPublication) => {
+      handleTrackPublished(trackPublication, participant);
+    });
+  }
 
   // TODO: Complete function for handling when a new participant track is published
-  function handleTrackPublished(trackPublication, participant) {}
+  function handleTrackPublished(trackPublication, participant) {
+    const yarn = document.getElementById(`yarn-${participant.number}`);
+    function handleTrackSubscribed(track) {
+      yarn.appendChild(track.attach());
+    }
+    if (trackPublication.track) {
+      handleTrackSubscribed(trackPublication.track);
+    }
+    trackPublication.on("subscribed", handleTrackSubscribed);
+  }
 
   // tidy up helper function for when a participant disconnects
   // or closes the page
